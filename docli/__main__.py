f86a6f0620d6653950104c7738eba2ee9ecdd26e
Jacob Errington <git@mail.jerrington.me>
2026-03-27 08:28:15 -0400

    todo and habit paths are relative

--- docli/__main__.py
+++ docli/__main__.py
@@ -19,11 +19,19 @@ def die(msg):
 
 BOT_TOKEN = getenv('BOT_TOKEN') or die('missing BOT_TOKEN env var')
 VAULT_DIR = getenv('VAULT_DIR') or die('missing VAULT_DIR env var')
-TODO_PATH = getenv('TODO_PATH') or os.path.join(VAULT_DIR, 'Todo.md')
+
+TODO_PATH = getenv('TODO_PATH')
+TODO_PATH = os.path.join(VAULT_DIR, TODO_PATH) \
+    if TODO_PATH else os.path.join(VAULT_DIR, 'Todo.md')
+
 HABITS_PATH = getenv('HABITS_PATH') or os.path.join(VAULT_DIR, 'Habits.md')
+HABITS_PATH = os.path.join(VAULT_DIR, HABITS_PATH) \
+    if HABITS_PATH else os.path.join(VAULT_DIR, 'Habits.md')
+
 TASK_TAG = getenv('TASK_TAG') or ''
-CHANNEL_NAME = getenv('CHANNEL_NAME') or die('missing CHANNEL_NAME env var')
 if TASK_TAG: TASK_TAG += ' '
+
+CHANNEL_NAME = getenv('CHANNEL_NAME') or die('missing CHANNEL_NAME env var')
 
 RESPONSES = [
     'OK boss 👍',
